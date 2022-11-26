d2913e9cb550b519d665af2aaa907a2bc67df839
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-11-26 10:33:17 +0100

    Infinix-X6816: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: infinix Model: Infinix X6816 Script version: 3.0.0

---
+++ omni_Infinix-X6816.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from Infinix-X6816 device
+$(call inherit-product, device/infinix/Infinix-X6816/device.mk)
+
+PRODUCT_DEVICE := Infinix-X6816
+PRODUCT_NAME := omni_Infinix-X6816
+PRODUCT_BRAND := Infinix
+PRODUCT_MODEL := Infinix X6816
+PRODUCT_MANUFACTURER := infinix
+
+PRODUCT_GMS_CLIENTID_BASE := android-infinix
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vnd_x6816_xq683-user 11 RP1A.200720.011 254692 release-keys"
+
+BUILD_FINGERPRINT := Infinix/X6816-OP/Infinix-X6816:11/RP1A.200720.011/220310V217:user/release-keys
---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_Infinix-X6816-user
+add_lunch_combo omni_Infinix-X6816-userdebug
+add_lunch_combo omni_Infinix-X6816-eng
